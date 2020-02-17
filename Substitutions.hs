c24a83f0b4ae59a4e5b025525f8b16cb9ae47a80
Zombibjarne <hallo.bjarne@gmail.com>
2020-02-17 17:10:55 +0100

    Added instance Vars for Subst

--- Substitutions.hs
+++ Substitutions.hs
@@ -3,16 +3,27 @@ module Substitutions where
 import Type
 import Prettyprinting
 import Data.List
+import Vars
 
 
+-- Instanciating Subst
 data Subst = Subst [(VarName, Term)]
   deriving Show
 
+instance Pretty Subst where
+  pretty (Subst xs) = "{" ++ (intercalate ", " (fmap (\(x, y) -> x ++ " -> " ++ (pretty y)) xs)) ++ "}"
+
+instance Vars Subst where
+  allVars (Subst xs) = killDuplicates (foldr (++) [] (fmap (\(x, y) -> x:allVars(y)) xs))
+
+
+-- Special Substitutions
 empty :: Subst
 empty = Subst []
 
 single :: VarName -> Term -> Subst
 single v t = Subst [(v, t)]
+
 
 -- Applies A Substitution to a Term
 apply :: Subst -> Term -> Term
@@ -30,8 +41,6 @@ compose (Subst xs) (Subst ys) = Subst (substitutedSet ++ filteredSet) where
   -- Filters the substitutions from the first substitution out of the second (based on the vars on the left side)
   filteredSet = filter (\(x, y) -> (not (elem x (fmap fst xs)))) ys
 
-instance Pretty Subst where
-  pretty (Subst xs) = "{" ++ (intercalate ", " (fmap (\(x, y) -> x ++ " -> " ++ (pretty y)) xs)) ++ "}"
 
 -- Substitutions for Testing
 s1 = Subst [("A",Comb "f" [Var "B", Var "_", Comb "true" []])]
