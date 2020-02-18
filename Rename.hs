d823eeae710907567829d6cc5e25a9309efc1454
Erik Steffahn <Erik.Steffahn@t-online.de>
2020-02-18 14:43:03 +0100

    changed replaceUnderscore

--- Rename.hs
+++ Rename.hs
@@ -5,8 +5,8 @@ import Vars
 
 
 -- Replaces the "_" Variable with other fitting variables
-replaceUnderscore :: Term -> Term
-replaceUnderscore t = fst (replaceAcc t (allVars t)) where
+replaceUnderscore :: Term -> [VarName] -> (Term, [VarName])
+replaceUnderscore t vs = replaceAcc t ((allVars t) ++ vs) where
     -- Replaces a "_" with a free variable and returns a tuple containing
     -- the new Term and the new List with variables that are now used in the term
     replaceAcc :: Term -> [VarName] -> (Term, [VarName])
@@ -17,27 +17,20 @@ replaceUnderscore t = fst (replaceAcc t (allVars t)) where
     replaceAcc (Var x) vs = ((Var x), vs)
     -- Replace within list
     replaceAcc (Comb f xs) vs = ((Comb f (fst rlist)), snd rlist) where
-        rlist = replaceList xs vs
+        rlist = convertType (replaceList xs vs)
         -- Same as replaceAcc but for Termlists instead
-        replaceList :: [Term] -> [VarName] -> ([Term], [VarName])
-        replaceList [] vs = ([], vs)
-        replaceList (x:xs) vs = ((firstTerm:finalTerms), finalVs) where
-            -- The result of the term replacement from the first term of the list
-            firstResult :: (Term, [VarName])
-            firstResult = (replaceAcc x vs)
-            firstVs :: [VarName]
-            firstVs = snd firstResult
-            firstTerm :: Term
-            firstTerm = fst firstResult
-            -- The result from replacing all other terms of the list
-            otherResult ::  ([Term], [VarName])
-            otherResult = replaceList xs firstVs
-            finalTerms :: [Term]
-            finalTerms = fst otherResult
-            finalVs :: [VarName]
-            finalVs = snd otherResult
+        replaceList :: [Term] -> [VarName] -> [(Term, [VarName])]
+        replaceList [] vs = []
+        replaceList (x:xs) vs = let y = replaceAcc x vs in (y):(replaceList xs (vs ++ (snd y)))
+        -- converts the output of replaceList into a type, that is useful for replaceAcc
+        convertType :: [(a,[b])] -> ([a],[b])
+        convertType [] = ([],[])
+        convertType (x:xs) = combine x (convertType xs)
+        combine (a1, b1s) (a2s, b2s) = (a1:a2s, b1s ++ b2s)
 
 -- What varnames not to pick for the renaming of the variable in given rule
 -- Returns a tuple containing the renamed rule and the superset of the fiven varname containing
 --  containing also the variable names
-rename :: [VarName] -> Rule -> Rule
+-- rename :: Rule -> [VarName] -> (Rule, [VarName])
+
+--(Comb "." [Var "_", Comb "true" [], Comb "j" [Var "_"]])
