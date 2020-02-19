3cd93dafc14c06c51df221abfb2dce5dac4251e5
Erik Steffahn <Erik.Steffahn@t-online.de>
2020-02-19 13:55:14 +0100

    fixed pretty, hopefully for the last time

--- prettyprinting.hs
+++ prettyprinting.hs
@@ -11,11 +11,11 @@ class Pretty a where
 instance Pretty Term where
   pretty (Var v) = v
   pretty (Comb c []) = c
-  pretty (Comb "." [Comb c1 xs, Comb "[]" []]) = "[" ++ (pretty (Comb c1 xs)) ++ "]"
-  pretty (Comb "." [Comb c1 xs, Comb "." ys]) = "[" ++ (intercalate ", " (c1 : toList (Comb "." ys))) ++ "]" where 
+  pretty (Comb "." [Comb c xs, Comb "[]" []]) = "[" ++ (pretty (Comb c xs)) ++ "]"
+  pretty (Comb "." [Comb c xs, Comb "." ys]) = "[" ++ (intercalate ", " (c : toList (Comb "." ys))) ++ "]" where 
    toList :: Term -> [String]
-   toList (Comb "." [Comb c1 xs, Comb "[]" []]) = [pretty (Comb c1 xs)]
-   toList (Comb "." [Comb _ xs, Comb "." _]) = (fmap pretty xs)
+   toList (Comb "." [Comb c xs, Comb "[]" []]) = [pretty (Comb c xs)]
+   toList (Comb "." [Comb c xs, Comb "." ys]) = (pretty (Comb c xs)):(toList (Comb "." ys))
    toList (Comb "." [x, y]) = [(pretty x) ++ "|" ++ (pretty y)]
    toList x = [pretty x]
   pretty (Comb "." [x, y]) = "[" ++ (pretty x) ++ "|" ++ (pretty y) ++ "]"
