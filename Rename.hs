9cd25ae391757e1b1f9ee54e367ee824e6b4566e
Erik Steffahn <Erik.Steffahn@t-online.de>
2020-02-21 18:05:01 +0100

    tried implementing State Monad into Rename, old version is still there

--- Rename.hs
+++ Rename.hs
@@ -4,7 +4,74 @@ import Data.List
 import Type
 import Vars
 import Substitutions
+import Control.Monad.State.Lazy
 
+-- rename will rename all variables in the type, while giving each "_" variable a unique name
+-- a list of varNames, that shouldn't be used in the renaming process can be inputted and the function will return a superset
+-- of that list, that will contain all the new substituted variables
+class Renameable a where
+  rename :: a -> State [VarName] a
+
+-- Replaces the "_" variable with other fitting variables, the variables in the specified list won't be used
+replaceUnderscore :: Term -> State [VarName] Term
+-- The actual replacing
+replaceUnderscore (Var "_") = state (\vs -> let newVar = head (filter (\x -> (not (elem x vs))) freshVars) in (Var newVar, newVar:vs))
+-- Nothing to replace
+replaceUnderscore (Var x) = state (\vs -> ((Var x), vs))
+-- Replace within list
+replaceUnderscore (Comb f xs) = state (\vs -> let rlist = runState (replaceList xs) vs in ((Comb f (fst rlist)), snd rlist))
+  
+replaceList :: [Term] -> State [VarName] [Term]
+replaceList ys = foldr (flip (>>=)) (pure []) (fmap (\t -> (\xs -> state (\vs -> let (x, v) = runState (replaceUnderscore t) vs in (x:xs, v)))) ys)
+
+-- Renames all variables in a rule, variables from the specified list won't be used
+-- returns the changed Rule and a superset of the input list, including all variables in the changed Rule
+instance Renameable Rule where 
+  rename r = state f where
+    f vnames = (apply (multiple ruleVars (fmap (\x -> (Var x)) substVars)) rule, nub(vnames ++ substVars)) where
+      -- replaces all underscore variables first
+      (rule, vars) = runState (replaceUnderscoreRule r) vnames
+      -- a list of all variables in the Rule
+      ruleVars = allVars rule
+      -- a list of all variables, that will be used in the substituted Rule
+      substVars = take (length ruleVars) (filter (\x -> not (elem x vars)) freshVars)
+      -- replaces all underscore variables in a Rule
+      replaceUnderscoreRule :: Rule -> State [VarName] Rule
+      replaceUnderscoreRule (Rule t ts) = (replaceUnderscore t) >>= (\x -> (state (\vs -> let (xs, v) = runState (replaceList ts) vs in (Rule x xs, v))))
+
+
+instance Renameable Term where
+  rename t = state (\vs -> let (Rule x _, ys) = runState (rename (Rule t [])) vs in (x, ys))
+
+instance Renameable Goal where
+  rename (Goal ts) = state (\vs -> let (ts', v) = runState (rename ts) vs in (Goal ts', v))
+
+instance Renameable Prog where
+  rename (Prog rs) = state (\vs -> let (rs', v) = runState (rename rs) vs in (Prog rs', v))
+
+instance (Renameable a) => Renameable [a] where
+  rename xs = foldr (flip (>>=)) (pure []) (fmap (\x -> (\ys -> state (\vs -> let (y, v) = runState (rename x) vs in (y:ys, v)))) xs)
+
+-- helper functions to get the inside of Prog, Goal, Rule
+fromProg :: Prog -> [Rule]
+fromProg (Prog rs) = rs
+
+fromGoal :: Goal -> [Term]
+fromGoal (Goal ts) = ts
+
+fromRule :: Rule -> [Term]
+fromRule (Rule t ts) = t:ts  
+
+{-module Rename where
+
+import Data.List
+import Type
+import Vars
+import Substitutions
+
+-- rename will rename all variables in the type, while giving each "_" variable a unique name
+-- a list of varNames, that shouldn't be used in the renaming process can be inputted and the function will return a superset
+-- of that list, that will contain all the new substituted variables
 class Renameable a where
   rename :: a -> [VarName] -> (a, [VarName])
 
@@ -84,3 +151,4 @@ fromGoal (Goal ts) = ts
 
 fromRule :: Rule -> [Term]
 fromRule (Rule t ts) = t:ts  
+-}
