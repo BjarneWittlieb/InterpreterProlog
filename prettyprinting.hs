f6320776fc8dc5cfa26e72b8c4e89211cd0ee11d
Erik Steffahn <Erik.Steffahn@t-online.de>
2020-02-21 18:38:38 +0100

    improved pretty

--- prettyprinting.hs
+++ prettyprinting.hs
@@ -14,21 +14,11 @@ instance Pretty Term where
   pretty (Var v) = v
   -- if the Term is a constant, return it
   pretty (Comb c []) = c
-  -- if the term is an array with only one element, return the element in brackets []
-  pretty (Comb "." [Comb c xs, Comb "[]" []]) = "[" ++ (pretty (Comb c xs)) ++ "]"
-  -- -if the term is an arry with several elements, get the list with the toList function
-  pretty (Comb "." [Comb f ts, Comb "." zs]) = "[" ++ (intercalate ", " (toList (Comb "." [Comb f ts, Comb "." zs]))) ++ "]" where 
-    toList :: Term -> [String]
-    -- if the list has only one element, return a list with that element
-    toList (Comb "." [Comb c xs, Comb "[]" []]) = [pretty (Comb c xs)]
-    -- if the list has more than one element, go through the list one by one
-    toList (Comb "." [Comb c xs, Comb "." ys]) = (pretty (Comb c xs)):(toList (Comb "." ys))
-    -- if the second argument isn't another list, it's a list comprehension
-    toList (Comb "." [x, y]) = [(pretty x) ++ "|" ++ (pretty y)]
-    -- otherwise it's a function, so just call pretty
-    toList x = [pretty x]
-  -- if the second argument isn't another list, it's a list comprehension
-  pretty (Comb "." [x, y]) = "[" ++ (pretty x) ++ "|" ++ (pretty y) ++ "]"
+  pretty (Comb "." [x, y]) = "[" ++ (intercalate ", " (toList (x, y))) ++ "]" where
+    toList :: (Term, Term) -> [String]
+    toList (x, Comb "[]" []) = [pretty x]
+    toList (x, Comb "." [y, z]) = (pretty x):(toList (y, z))
+    toList (x, y) = [(pretty x) ++ "|" ++ (pretty y)]
   -- otherwise it's a function, so put the function name first and the arguments in brackets ()
   pretty (Comb c xs) = c ++ "(" ++ (intercalate ", " (fmap pretty xs)) ++ ")" 
 
