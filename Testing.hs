2691d9a4bab583f3c92b464219ada6e863c6623f
Zombibjarne <hallo.bjarne@gmail.com>
2020-02-27 15:57:28 +0100

    Implemented some tests

--- Testing.hs
+++ Testing.hs
@@ -45,12 +45,44 @@ instance Eq Peano where
     (S x) == (S y)  = x == y
     _ == _          = False
 
-prop_one_solution :: Peano -> Peano -> Bool
-prop_one_solution x y = case solve dfs peanoProgram (Goal [(Comb "add" [(peanoToTerm x) ,(peanoToTerm y), (Var "X")])]) of
+one_solution :: Strategy -> Peano -> Peano -> Bool
+one_solution strat x y = case solve strat peanoProgram (Goal [(Comb "add" [(peanoToTerm x) ,(peanoToTerm y), (Var "X")])]) of
     [(Subst [("X", term)])] -> case termToPeano term of
         Just p -> p == (addP x y)
         Nothing -> False
     _ -> False
+
+prop_dfs_one_solution = one_solution dfs
+prop_bfs_one_solution = one_solution bfs
+prop_idfs_one_solution = one_solution idfs
+
+
+twoVarsTerm :: Goal
+twoVarsTerm = case parse "=(A, B)" of
+    Right g -> g
+    _       -> error "HELP"
+unify_twoVars :: Strategy -> Bool
+unify_twoVars strat = case solve strat (Prog []) twoVarsTerm of
+    [Subst [("A", (Var "B"))]] -> True
+    [Subst [("B", (Var "A"))]] -> True
+    _ -> False
+
+prop_dfs_unify_twoVars = unify_twoVars dfs
+prop_bfs_unify_twoVars = unify_twoVars bfs
+prop_idfs_unify_twoVars = unify_twoVars idfs
+
+occurTerm :: Goal
+occurTerm = case parse "=(A, f(A))" of
+    Right g -> g
+    _       -> error "HELP"
+unify_occur :: Strategy -> Bool
+unify_occur strat = case solve strat (Prog []) occurTerm of
+    [] -> True
+    _ -> False
+
+prop_dfs_unify_occur = unify_occur dfs
+prop_bfs_unify_occur = unify_occur bfs
+prop_idfs_unify_occur = unify_occur idfs
 
 return []
 runTests = $quickCheckAll
